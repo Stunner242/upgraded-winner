5b874f753d678d079fa99f755f254af5c0b8c494
Vishva Pandey <99552212+Stunner242@users.noreply.github.com>
2024-07-26 15:34:31 +0530

    Update RockPaperScissor.cpp

--- RockPaperScissor.cpp
+++ RockPaperScissor.cpp
@@ -1,93 +1,85 @@
 #include <iostream>
-#include <cstdlib> 
-#include <ctime>  
+#include <ctime>
+#include <cstdlib>
 
 using namespace std;
 
-enum Choice { Rock, Paper, Scissors };
+enum Options { Rock, Paper, Scissors };
 
-// Function to get computer's choice (random)
-Choice getComputerChoice() {
-  // Seed the random number generator (improves randomness)
-  srand(time(0));
-  // Generate a random number between 1 and 3
-  int random_number = rand() % 3;
-  // Convert random number to Choice enum
-  return static_cast<Choice>(random_number);
-}
-
-// Function to get player's choice (validated input)
-Choice getPlayerChoice() {
-  char input;
-  cout << "Enter your choice (R - Rock, P - Paper, S - Scissors): ";
-  cin >> input;
-
-  // Validate input (convert to uppercase for easier comparison)
-  input = toupper(input);
-  while (input != 'R' && input != 'P' && input != 'S') {
-    cout << "Invalid input. Please try again: ";
+Options user() {
+    char input;
+    cout << "Enter your Choice to proceed: (R- Rock, P- Paper, S- Scissors): ";
     cin >> input;
-    input = toupper(input);
-  }
 
-  // Convert valid input to Choice enum
-  switch (input) {
-    case 'R':
-      return Rock;
-    case 'P':
-      return Paper;
-    case 'S':
-      return Scissors;
-  }
-  return Rock; // Should never reach here (compiler may warn)
+    input = toupper(input);
+    while (input != 'R' && input != 'P' && input != 'S') {
+        cout << "Invalid input. Please try again: ";
+        cin >> input;
+        input = toupper(input);
+    }
+
+    switch (input) {
+        case 'R':
+            return Rock;
+        case 'P':
+            return Paper;
+        case 'S':
+            return Scissors;
+        default:
+            return Rock; 
+    }
 }
 
-// Function to determine the winner
-int determineWinner(Choice playerChoice, Choice computerChoice) {
-  if (playerChoice == computerChoice) {
-    return 0; // Tie
-  } else if ((playerChoice == Rock && computerChoice == Scissors) ||
-             (playerChoice == Paper && computerChoice == Rock) ||
-             (playerChoice == Scissors && computerChoice == Paper)) {
-    return 1; // Player wins
-  } else {
-    return -1; // Player loses
-  }
+Options comp() {
+    srand(time(0)); 
+    int random_num = rand() % 3;
+    return static_cast<Options>(random_num);
+}
+
+int winner(Options user, Options comp) {
+    if (user == comp) {
+        return 0; 
+    } else if ((user == Rock && comp == Scissors) ||
+              (user == Paper && comp == Rock) ||
+              (user == Scissors && comp == Paper)) {
+        return 1; 
+    } else {
+        return -1; 
+    }
 }
 
 int main() {
-  int playerScore = 0;
-  int computerScore = 0;
-
-  while (true) {
-    Choice computerChoice = getComputerChoice();
-    Choice playerChoice = getPlayerChoice();
-
-    cout << "Your choice: " << (playerChoice == Rock ? "Rock" : (playerChoice == Paper ? "Paper" : "Scissors")) << endl;
-    cout << "Computer's choice: " << (computerChoice == Rock ? "Rock" : (computerChoice == Paper ? "Paper" : "Scissors")) << endl;
-
-    int result = determineWinner(playerChoice, computerChoice);
-    if (result == 0) {
-      cout << "Tie!" << endl;
-    } else if (result == 1) {
-      cout << "You win!" << endl;
-      playerScore++;
-    } else {
-      cout << "You lose!" << endl;
-      computerScore++;
-    }
-
-    cout << "Your score: " << playerScore << ", Computer score: " << computerScore << endl;
-
+    int userScore = 0;
+    int compScore = 0;
     char playAgain;
-    cout << "Play again? (y/n): ";
-    cin >> playAgain;
-    if (toupper(playAgain) != 'Y') {
-      break;
-    }
-  }
 
-  cout << "Thanks for playing!" << endl;
+    do {
+        cout << "Welcome to Rock, Paper, Scissors\n";
 
-  return 0;
+        Options userChoice = user();
+        Options compChoice = comp();
+
+        cout << "Your choice: " << (userChoice == Rock ? "Rock" : (userChoice == Paper ? "Paper" : "Scissors")) << endl;
+        cout << "Computer's choice: " << (compChoice == Rock ? "Rock" : (compChoice == Paper ? "Paper" : "Scissors")) << endl;
+
+        int result = winner(userChoice, compChoice);
+        if (result == 0) {
+            cout << "Tie!" << endl;
+        } else if (result == 1) {
+            cout << "You win!" << endl;
+            userScore++;
+        } else {
+            cout << "You lose!" << endl;
+            compScore++;
+        }
+
+        cout << "Your score: " << userScore << ", Computer score: " << compScore << endl;
+
+        cout << "Play again? (y/n): ";
+        cin >> playAgain;
+    } while (toupper(playAgain) == 'Y');
+
+    cout << "Thanks for playing!" << endl;
+
+    return 0;
 }
