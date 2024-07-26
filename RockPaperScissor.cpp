5fb5f4923c85501396aee5bc4c49f0856849681b
Vishva Pandey <99552212+Stunner242@users.noreply.github.com>
2024-07-26 14:59:40 +0530

    Add files via upload

---
+++ RockPaperScissor.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+#include<stdarg.h>
+#include<math.h>
+#include<iostream>
+#include<ctime>
+using namespace std;
+
+enum Options {Rock,
+ Paper, Scissors };
+ Options compOptions(){
+ 	int random_num = rand()%3;
+ }
+ 
+ 
